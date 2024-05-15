dab59d1543e0c01981cd893b8a734f898c293515
bahou1 <77anwarrr@gmail.com>
2024-05-15 21:10:22 +0100

    made the side menu fixed while scrolling

--- arkxfinal/src/pages/Dashboard.jsx
+++ arkxfinal/src/pages/Dashboard.jsx
@@ -5,11 +5,11 @@ import CustomerTable from '../components/CustomerTable';
 
 const Dashboard = () => {
     return (
-        <div className="bg-gray-200 min-h-screen flex gap-9 ">
-            <div className="">
+        <div className="bg-red-200 min-h-screen flex gap-9 ">
+            <div className="fixed"> {/* Add the fixed class here */}
                 <SideMenu/>
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col absolute right-4 gap-4"> {/* Add margin-left to compensate for the fixed SideMenu */}
                 <SearchDash/>
                 <CustomerTable/>
             </div>
