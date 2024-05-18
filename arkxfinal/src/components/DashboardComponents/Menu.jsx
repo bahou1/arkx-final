86d81b57f16c73798964ae792aa8e3072b56b2a2
bahou1 <77anwarrr@gmail.com>
2024-05-18 14:47:40 +0100

    added popup animations

--- arkxfinal/src/components/DashboardComponents/Menu.jsx
+++ arkxfinal/src/components/DashboardComponents/Menu.jsx
@@ -1,125 +1,138 @@
-import React from 'react';
-import { AiOutlineHeart, AiOutlineEdit, AiOutlineEye, AiOutlineDelete } from 'react-icons/ai'; // Import the icons
+import React, { useState, useEffect } from 'react';
+import { AiOutlineHeart, AiOutlineEdit, AiOutlineEye, AiOutlineDelete } from 'react-icons/ai';
+import { AiOutlineClose, AiOutlineDollar, AiOutlineStar } from 'react-icons/ai';
 
 const Menu = () => {
-        // Define the data as a constant
-        const data = {
-            "menu": [
-                {
-                    "_id": "66410510159a6e7e2deb3e72",
-                    "title": "moroccan-couscous",
-                    "price": "150.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/moroccan-couscous.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e73",
-                    "title": "zaalouk",
-                    "price": "120.00 MAD",
-                    "rating": 3,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/zaalouk.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e74",
-                    "title": "what-to-eat-in-morocco",
-                    "price": "200.00 MAD",
-                    "rating": 5,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/what-to-eat-in-morocco.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e75",
-                    "title": "harira-moroccan-food",
-                    "price": "180.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2019/10/harira-moroccan-food.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e76",
-                    "title": "sheep-head-food-morocco",
-                    "price": "250.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/sheep-head-food-morocco.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e77",
-                    "title": "sardines-chermoula",
-                    "price": "170.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/sardines-chermoula.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e78",
-                    "title": "moroccan-mint-tea",
-                    "price": "50.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/moroccan-mint-tea.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e79",
-                    "title": "moroccan-brochettes",
-                    "price": "160.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/moroccan-brochettes.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e80",
-                    "title": "snail-soup-morocco",
-                    "price": "220.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/snail-soup-morocco.jpg"
-                },
-                {
-                    "_id": "66410510159a6e7e2deb3e81",
-                    "title": "tanjia",
-                    "price": "200.00 MAD",
-                    "rating": 4,
-                    "imgUrl": "https://localadventurer.com/wp-content/uploads/2015/11/tanjia.jpg"
-                }
-            ]
+    const [menuData, setMenuData] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [showConfirmation, setShowConfirmation] = useState(false);
+    const [identifierToDelete, setIdentifierToDelete] = useState(null);
+
+    useEffect(() => {
+        const fetchMenuData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/menu');
+                const data = await response.json();
+                setMenuData(data);
+            } catch (error) {
+                console.error('Error fetching menu data:', error);
+            }
+        };
+
+        fetchMenuData();
+    }, []);
+
+    const handleView = (item) => {
+        setSelectedItem(item);
     };
 
+    const handleCloseModal = () => {
+        setSelectedItem(null);
+    };
 
-        return (
-<div className="w-full gap-9 mt-4 mb-4 grid grid-cols-9 sm:grid-cols-9 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 pl-3 pr-3">
-    {data.menu.map(item => (
-        <div key={item._id} className="flex flex-col items-center gap-3 relative bg-white rounded-3xl pb-4">
-            <div className="relative rounded-full w-64 h-64 bg-cover bg-center " style={{ backgroundImage: `url(${item.imgUrl})` }}>
+    const handleDelete = async (identifier) => {
+        console.log('Deleting item with identifier:', identifier); // Log the identifier
+    
+        try {
+            // Send DELETE request to backend
+            const response = await fetch(`http://localhost:3000/api/menu/${identifier}`, {
+                method: 'DELETE',
+            });
+    
+            console.log('Response:', response); // Log the response
+    
+            if (!response.ok) {
+                throw new Error('Failed to delete item');
+            }
+    
+            // Remove the item from the UI
+            setMenuData(prevMenuData => {
+                const newMenuData = prevMenuData.filter(item => item.identifier !== identifier);
+                console.log('New menu data:', newMenuData); // Log the new state
+                return newMenuData;
+            });
+        } catch (error) {
+            console.error('Error deleting menu item:', error);
+        }
+    };
 
-            </div>
-            <p className="text-xl font-lato font-medium">{item.price}</p>
-            <div className="flex gap-4 mt-8">
-                <div className="flex flex-col items-center">
-                    <button 
-                        className="rounded-2xl w-12 h-12 font-lato font-3xl flex items-center justify-center transition-colors duration-200 ease-in-out" 
-                        style={{ backgroundColor: '#FFF0ED', color: '#FF6D4D' }}
-                    >
-                        <AiOutlineEdit size={23} />
-                    </button>
-                    <p>Edit</p>
+    const handleDeleteConfirmation = (identifier) => {
+        setShowConfirmation(true);
+        setIdentifierToDelete(identifier);
+    };
+
+    const handleConfirmDelete = async () => {
+        try {
+            await handleDelete(identifierToDelete);
+            setShowConfirmation(false);
+        } catch (error) {
+            console.error('Error confirming deletion:', error);
+        }
+    };
+
+    const handleCancelDelete = () => {
+        setShowConfirmation(false);
+    };
+
+    return (
+        <div className="w-full gap-9 mt-4 mb-4 grid grid-cols-9 sm:grid-cols-9 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 pl-3 pr-3">
+            {menuData.map(item => (
+                <div key={item._id} className="flex flex-col items-center gap-3 relative bg-white rounded-3xl pb-4">
+                    <div className="relative rounded-full w-64 h-64 bg-cover bg-center " style={{ backgroundImage: `url(${item.imgUrl})` }}></div>
+                    <h2 className="text-xl font-lato font-medium text-center">{item.title}</h2>
+                    <p className="text-xl font-lato font-medium">{item.price}</p>
+                    <div className="flex gap-4 mt-8">
+                        <div className="flex flex-col items-center">
+                            <button className="rounded-2xl w-12 h-12 font-lato font-3xl flex items-center justify-center transition-colors duration-200 ease-in-out" style={{ backgroundColor: '#FFF0ED', color: '#FF6D4D' }}>
+                                <AiOutlineEdit size={23} />
+                            </button>
+                            <p>Edit</p>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <button onClick={() => handleView(item)} className="rounded-2xl w-12 h-12 font-lato font-3xl flex items-center justify-center transition-colors duration-200 ease-in-out" style={{ backgroundColor: '#DFF0FA', color: '#2D9CDB' }}>
+                                <AiOutlineEye size={23} />
+                            </button>
+                            <p>View</p>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <button onClick={() => handleDeleteConfirmation(item.identifier)} className="rounded-2xl w-12 h-12 font-lato font-3xl flex items-center justify-center transition-colors duration-200 ease-in-out" style={{ backgroundColor: '#D9F3EA', color: '#00B074' }}>
+                                <AiOutlineDelete size={23} />
+                            </button>
+                            <p>Delete</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex flex-col items-center">
-                    <button 
-                        className="rounded-2xl w-12 h-12 font-lato font-3xl flex items-center justify-center transition-colors duration-200 ease-in-out" 
-                        style={{ backgroundColor: '#DFF0FA', color: '#2D9CDB' }}
-                    >
-                        <AiOutlineEye size={23} />
-                    </button>
-                    <p>View</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <button 
-                        className="rounded-2xl w-12 h-12 font-lato font-3xl flex items-center justify-center transition-colors duration-200 ease-in-out" 
-                        style={{ backgroundColor: '#D9F3EA', color: '#00B074' }}
-                    >
-                        <AiOutlineDelete size={23} />
-                    </button>
-                    <p>Delete</p>
-                </div>
+            ))}
+{selectedItem && (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50 transition-opacity duration-300 font-lato">
+        <div className="bg-white p-6 rounded-lg max-w-md transform transition-all duration-300 opacity-100 shadow-lg border-4 border-gray-300 animate-popup">
+            <button onClick={handleCloseModal} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+                <AiOutlineClose size={24} />
+            </button>
+            <h2 className="text-2xl font-semibold mb-4">{selectedItem.title}</h2>
+            <img src={selectedItem.imgUrl} alt={selectedItem.title} className="mt-2 mb-4 rounded-lg w-full shadow-md" />
+            <p className="space-x-2 text-lg mb-2 flex items-center"><AiOutlineDollar /><span> Price: <span className="font-semibold">{selectedItem.price}</span></span></p>
+            <p className="space-x-2 text-lg mb-2 flex items-center"><AiOutlineStar /><span> Rating: <span className="font-semibold">{selectedItem.rating}</span></span></p>
+            <p className="space-x-2 text-lg mb-2 flex items-center"><AiOutlineStar /><span> Identifier: <span className="font-semibold">{selectedItem.identifier}</span></span></p>
+
+            <p className="text-lg">{selectedItem.description}</p>
+        </div>
+    </div>
+)}
+{showConfirmation && (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50 transition-opacity duration-300">
+        <div className="bg-white p-6 rounded-lg max-w-md transform transition-all duration-300 opacity-100 shadow-lg border-4 border-gray-300 animate-popup">
+            <p className="text-lg font-semibold mb-4">Are you sure you want to delete this item?</p>
+            <div className="flex justify-end">
+                <button onClick={handleCancelDelete} className="px-4 py-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300 focus:outline-none focus:bg-gray-300 mr-2">Cancel</button>
+                <button onClick={handleConfirmDelete} className="px-4 py-2 bg-lime-700 text-white rounded-md hover:bg-lime-800 focus:outline-none focus:bg-lime-700">Confirm</button>
             </div>
         </div>
-    ))}
-</div>
-        );
-};
+    </div>
+)}
 
-export default Menu;
+            </div>
+        );
+    };
+    
+    export default Menu;    
